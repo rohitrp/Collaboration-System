71218c33bf00f35f6843b1d19c50c91a04c81de8
Rohit Prasad <rohitrp3@gmail.com>
2018-03-16 16:48:41 +0530

    Add tests for inappropriate content filter

--- InappropriateContentFilter/tests.py
+++ InappropriateContentFilter/tests.py
@@ -1,3 +1,37 @@
 from django.test import TestCase
+from .views import inappropriate_content_filter
+import pytest
 
-# Create your tests here.
+class InappropriateContentFilterTest(TestCase):
+    titles = []
+    bodies = []
+
+    def setUp(self):
+        self.titles = ["Hello", "hell"]
+        self.bodies = ["hello", "hell with it"]
+
+    def test_appropriate_content(self):
+        self.assertEqual(
+            inappropriate_content_filter(self.titles[0], self.bodies[0]), 
+            {'inappropriate': False, 'inappropriate_words': []}
+        )
+        
+        self.assertNotEqual(
+            inappropriate_content_filter(self.titles[0], self.bodies[0])['inappropriate'], 
+            True
+        )
+
+    def test_inappropriate_content(self):
+        self.assertEqual(
+            inappropriate_content_filter(self.titles[1], self.bodies[1]), 
+            {'inappropriate': True, 'inappropriate_words': ['hell', 'hell']}
+        )
+        
+        self.assertNotEqual(
+            inappropriate_content_filter(self.titles[1], self.bodies[0])['inappropriate'], 
+            False
+        )
+        
+    def test_incorrect_arguments(self):
+        with self.assertRaisesMessage(IndexError, 'tuple index out of range'):
+            inappropriate_content_filter(self.titles[0])
